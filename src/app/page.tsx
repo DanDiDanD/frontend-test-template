7cfbeefebe2caaff4b9623bfb22346710a2ee787
DanDiDanD <pfbarrett98@gmail.com>
2025-08-05 02:16:42 -0500

    fix(catalog): update page's title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import MainContainer from "@/components/layout/MainContainer";
 export default function Home() {
   return (
     <MainContainer className="py-8 md:py-12">
-      <h1 className="text-2xl md:text-4xl font-bold mb-8">Game Catalog</h1>
+      <h1 className="text-2xl md:text-4xl font-bold mb-8">Top Sellers</h1>
       <Suspense fallback={<GameSkeleton />}>
         <Catalog />
       </Suspense>
