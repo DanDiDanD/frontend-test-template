ad7164a7904ca7c80461e82bc98d4c292b1284c0
DanDiDanD <pfbarrett98@gmail.com>
2025-08-05 02:17:20 -0500

    fix(catalog): update games skeleton

--- src/components/features/catalog/GameSkeleton.tsx
+++ src/components/features/catalog/GameSkeleton.tsx
@@ -1,17 +1,27 @@
 export default function GameSkeleton() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-12 md:mb-12">
       {Array.from({ length: 12 }).map((_, i) => (
-        <div key={i} className="border rounded-lg p-4 shadow-md animate-pulse">
-          <div className="w-full h-48 bg-gray-300 rounded mb-4"></div>
-          <div className="h-6 bg-gray-300 rounded mb-2"></div>
-          <div className="h-4 bg-gray-200 rounded mb-2 w-3/4"></div>
-          <div className="h-4 bg-gray-200 rounded mb-4"></div>
-          <div className="flex justify-between items-center mb-4">
-            <div className="h-6 bg-gray-300 rounded w-16"></div>
-            <div className="h-6 bg-gray-200 rounded w-10"></div>
+        <div
+          key={i}
+          className="bg-white border border-gray-300 rounded-2xl px-6 py-6 flex flex-col animate-pulse"
+        >
+          <div className="aspect-[6/5] md:aspect-[4/3] relative mb-6">
+            <div className="w-full h-full bg-gray-300 rounded-t-lg"></div>
           </div>
-          <div className="h-10 bg-gray-300 rounded"></div>
+
+          <div className="flex flex-col flex-grow">
+            <div className="h-4 bg-gray-200 rounded mb-3 w-16"></div>
+
+            <div className="flex justify-between gap-2 mb-5">
+              <div className="h-5 bg-gray-300 rounded flex-1"></div>
+              <div className="h-5 bg-gray-300 rounded w-12"></div>
+            </div>
+
+            <div className="mt-auto">
+              <div className="h-12 bg-gray-200 rounded-lg w-full"></div>
+            </div>
+          </div>
         </div>
       ))}
     </div>
