a90de1fda8849597c47d03c277b4dac0897226f4
DanDiDanD <pfbarrett98@gmail.com>
2025-08-05 17:48:01 -0500

    fix(ui): align focus ring's color to button's color

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -12,17 +12,18 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
 
 const buttonStyles = {
   solid: {
-    primary: "bg-primary text-white hover:bg-primary/90",
-    "gray-medium": "bg-gray-medium text-white hover:bg-gray-medium/90",
-    danger: "bg-red-600 text-white hover:bg-red-600/90",
+    primary: "bg-primary text-white hover:bg-primary/90 focus:ring-primary",
+    "gray-medium":
+      "bg-gray-medium text-white hover:bg-gray-medium/90 focus:ring-gray-medium",
+    danger: "bg-red-600 text-white hover:bg-red-600/90 focus:ring-red-600",
   },
   outline: {
     primary:
-      "border border-primary text-primary hover:bg-primary hover:text-white",
+      "border border-primary text-primary hover:bg-primary hover:text-white focus:ring-primary",
     "gray-medium":
-      "border border-gray-medium text-gray-medium hover:bg-gray-medium hover:text-white",
+      "border border-gray-medium text-gray-medium hover:bg-gray-medium hover:text-white focus:ring-gray-medium",
     danger:
-      "border border-red-600 text-red-600 hover:bg-red-600 hover:text-white",
+      "border border-red-600 text-red-600 hover:bg-red-600 hover:text-white focus:ring-red-600",
   },
 };
 
@@ -36,7 +37,7 @@ export default function Button({
   ...props
 }: ButtonProps) {
   const baseClasses =
-    "px-5 py-4 md:px-6 md:py-5 text-sm md:text-base font-bold leading-4 rounded-lg transition-colors w-full md:w-auto focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary disabled:opacity-50 disabled:cursor-not-allowed";
+    "px-5 py-4 md:px-6 md:py-5 text-sm md:text-base font-bold leading-4 rounded-lg transition-colors w-full md:w-auto focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed";
   const variantClasses = buttonStyles[variant][color];
 
   const combinedClasses = `${baseClasses} ${variantClasses} ${className}`;
