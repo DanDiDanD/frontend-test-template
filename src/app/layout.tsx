4b3b3497b9b4cef5e529d2d53499d51dd4203343
DanDiDanD <pfbarrett98@gmail.com>
2025-08-04 23:38:05 -0500

    fix(layout): remove repeated main tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en" className={`${archivo.className} antialiased`}>
       <body>
         <Header />
-        <main>{children}</main>
+        {children}
         <Footer />
       </body>
     </html>
