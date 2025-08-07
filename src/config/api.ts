32e673e6730a5f573ae4cbe47568f28c6181b39d
DanDiDanD <pfbarrett98@gmail.com>
2025-08-06 21:57:07 -0500

    fix(api): set default API_BASE_URL value as empty string

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,4 @@
-const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "";
 
 export const API_ENDPOINTS = {
   GAMES: "/api/games",
